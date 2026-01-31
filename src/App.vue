a6df3d0940711d5a639e3b75b4dc469b87287eff
Sergey Nikiforov <ser.nikiforov@gmail.com>
2026-01-31 19:08:14 +0200

    Первый коммит

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import PWABadge from './components/PWABadge.vue'
+</script>
+
+<template>
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-xl">
+      <div class="flex min-h-[100svh] flex-col justify-between pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)]">
+        <div class="">Настройки</div>
+        <div class="">Уровень</div>
+        <div class="">Таймер</div>
+        <div class="">Предложение практики</div>
+        <div class="">Старт/Стоп</div>
+        <div class=""> </div>
+      </div>
+    </div>
+    <PWABadge />
+  </div>
+</template>
---
+++ src/components/PWABadge.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
+import { ArrowPathIcon } from '@heroicons/vue/24/outline'
+import { XMarkIcon } from '@heroicons/vue/20/solid'
+
+// check for updates every day
+const period = 24 * 60 * 60 * 1000
+
+const swActivated = ref(false)
+
+/**
+ * This function will register a periodic sync check every hour, you can modify the interval as needed.
+ */
+function registerPeriodicSync(swUrl: string, r: ServiceWorkerRegistration) {
+  if (period <= 0) return
+
+  setInterval(async () => {
+    if ('onLine' in navigator && !navigator.onLine)
+      return
+
+    const resp = await fetch(swUrl, {
+      cache: 'no-store',
+      headers: {
+        'cache': 'no-store',
+        'cache-control': 'no-cache',
+      },
+    })
+
+    if (resp?.status === 200)
+      await r.update()
+  }, period)
+}
+
+const { needRefresh, updateServiceWorker } = useRegisterSW({
+  immediate: true,
+  onRegisteredSW(swUrl, r) {
+    if (period <= 0) return
+    if (r?.active?.state === 'activated') {
+      swActivated.value = true
+      registerPeriodicSync(swUrl, r)
+    }
+    else if (r?.installing) {
+      r.installing.addEventListener('statechange', (e) => {
+        const sw = e.target as ServiceWorker
+        swActivated.value = sw.state === 'activated'
+        if (swActivated.value)
+          registerPeriodicSync(swUrl, r)
+      })
+    }
+  },
+})
+
+function close() {
+  needRefresh.value = false
+}
+</script>
+
+<template>
+  <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-start px-4 py-6 sm:p-6">
+    <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+      <transition
+        enter-active-class="transform ease-out duration-300 transition"
+        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        enter-to-class="translate-y-0 sm:translate-x-0"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class=""
+        leave-to-class="opacity-0"
+      >
+        <div
+          v-if="needRefresh"
+          class="pointer-events-auto w-full max-w-sm rounded-lg bg-gray-800 shadow-lg outline-1 -outline-offset-1 outline-white/10"
+          aria-labelledby="toast-message"
+          role="alert"
+        >
+          <div class="p-4">
+            <div class="flex items-start">
+              <div class="shrink-0">
+                <ArrowPathIcon class="size-6 text-gray-400" aria-hidden="true" />
+              </div>
+              <div class="ml-3 w-0 flex-1 pt-0.5">
+                <p id="toast-message" class="text-sm font-medium text-white">
+                  Доступно обновление
+                </p>
+                <p class="mt-1 text-sm text-gray-400">
+                  При обновлении все данные остаются без изменений
+                </p>
+                <div class="mt-3 flex space-x-7">
+                  <button
+                    type="button"
+                    class="rounded-md text-sm font-medium text-indigo-400 hover:text-indigo-300 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-400"
+                    @click="updateServiceWorker()"
+                  >
+                    Обновить
+                  </button>
+                  <button
+                    type="button"
+                    class="rounded-md text-sm font-medium text-gray-300 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-400"
+                    @click="close"
+                  >
+                    Пропустить
+                  </button>
+                </div>
+              </div>
+              <div class="ml-4 flex shrink-0">
+                <button
+                  type="button"
+                  class="inline-flex rounded-md text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
+                  @click="close"
+                >
+                  <span class="sr-only">Close</span>
+                  <XMarkIcon class="size-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+  </div>
+</template>
